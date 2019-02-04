891478651cd78f35b03d25f7b0aa0880457afff1
sirusdynamo <sirusdynamo@gmail.com>
2019-02-05 00:02:21 +0100

    test written for board view

--- boards/tests.py
+++ boards/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse, resolve
-from .views import home
+from .views import home , board_topics
+from .models import Board
 # Create your tests here.
 
 
@@ -19,3 +20,26 @@ class HomeTests(TestCase):
     def test_home_resolves_home_url_name(self):
         view =resolve('/')
         self.assertEqual(view.url_name,'home')
+
+class BoardTopicsTest(TestCase):
+
+    def setUp(self):
+        Board.objects.create(name='Django', description='A discusion about django')
+
+
+    def test_board_topics_success_status_code(self):
+        url = reverse('board_topics', kwargs={'board_id':1})
+        response =self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_board_topics_view_not_found_status_code(self):
+        url = reverse('board_topics', kwargs= {'board_id':89})
+        response =self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_board_topics_url_resolves_board_topics_view_(self):
+        view = resolve('/boards/1/')
+        self.assertEqual(view.func ,board_topics)
+
+
+
