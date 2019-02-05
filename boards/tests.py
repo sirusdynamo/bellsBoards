ecb12e49e98e6c1efb6d91bae3f2e2f3eafcf1ab
sirusdynamo <sirusdynamo@gmail.com>
2019-02-05 17:37:29 +0100

    test: added more test for home page

--- boards/tests.py
+++ boards/tests.py
@@ -6,11 +6,13 @@ from .models import Board
 
 
 class HomeTests(TestCase):
-    def test_home_view_status_code(self):
-        url=reverse('home')
-        response =self.client.get(url)
-        self.assertEqual(response.status_code, 200)
+    def setUp(self):
+        self.board =Board.objects.create(name='Django', description='Django boards ')
+        url = reverse('home')
+        self.response = self.client.get(url)
 
+    def test_home_view_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
 
 
     def test_home_url_resolves_home(self):
@@ -20,6 +22,12 @@ class HomeTests(TestCase):
     def test_home_resolves_home_url_name(self):
         view =resolve('/')
         self.assertEqual(view.url_name,'home')
+
+    def test_home_view_contains_link_to_topics_page(self):
+        board_topics_url = reverse('board_topics', kwargs={'board_id': self.board.pk})
+        self.assertContains(self.response, 'href="{0}"'.format(board_topics_url))
+
+
 
 class BoardTopicsTest(TestCase):
 
