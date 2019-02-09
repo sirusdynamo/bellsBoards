613f5c7f999bbbd21287f5ee948fcefe69f184e3
sirusdynamo <sirusdynamo@gmail.com>
2019-02-10 00:45:34 +0100

    Added form processing to view

--- boards/views.py
+++ boards/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render ,get_object_or_404
+from django.shortcuts import render ,get_object_or_404, redirect
 from django.http import HttpResponse
-from .models import Board
+from .models import Board ,User ,Topic, Post
+from .forms import NewTopicForm
 
 # Create your views here.
 def home(request):
@@ -14,4 +15,22 @@ def board_topics(request, board_id):
 
 def new_topics(request, board_id):
     board = get_object_or_404(Board, pk=board_id)
-    return render(request, 'new_topics.html', {'board':board})
+    user = User.objects.first()
+    if request.method == 'POST':
+        form = NewTopicForm(request.POST)
+        if form.is_valid():
+            topic = form.save(commit=False)
+            topic.board = board
+            topic.starter = user
+            topic.save()
+            Post.objects.create(
+                message = form.cleaned_data.get('message'),
+                topic = topic,
+                created_by = user
+            )
+            
+            return redirect('board_topics', board_id=board.id )
+
+    else:
+        form = NewTopicForm()
+    return render(request, 'new_topics.html', {'board':board, 'form': form})
