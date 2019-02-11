0021a380f6dcfeccb3977945d205171d8625e81f
sirusdynamo <sirusdynamo@gmail.com>
2019-02-11 19:55:54 +0100

    used django widget tweaks edit forms

--- myproject/settings.py
+++ myproject/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'boards.apps.BoardsConfig'
+    'boards.apps.BoardsConfig',
+    'widget_tweaks'
 ]
 
 MIDDLEWARE = [
